84c98003eefa960e1457e9f30c16814283ccd67e
christianekenstedt <christian>
2017-02-21 16:39:33 +0100

    Made gui basics and ranamed file

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,24 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.VBox;
+
 public class Controller {
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private VBox shapeView;
+
+    @FXML
+    private VBox propertiesView;
+
+
+
 }
