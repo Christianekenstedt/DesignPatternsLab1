da6369c2fb84654ab32db8cdfdbd8f1bd70afa85
antonwass <anton_wass@hotmail.com>
2017-02-21 17:09:36 +0100

    added model classes

---
+++ src/Model/Drawing.java
@@ -0,0 +1,11 @@
+package Model;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+/**
+ * Created by Anton on 2017-02-21.
+ */
+public class Drawing implements IDrawing{
+
+}
